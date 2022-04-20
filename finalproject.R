bd9f6dbd27cbe79a539d098d80d842601ea08621
regane2 <92222931+regane2@users.noreply.github.com>
2022-04-20 16:47:12 -0700

    slicing and dicing

--- finalproject.R
+++ finalproject.R
@@ -8,3 +8,22 @@ access_token <- get_spotify_access_token()
 swift <- get_artist_audio_features('taylor swift')
 View(swift)
 #slicing and dicing 
+swift %>% distinct(album_name)
+albums_to_keep <- swift %>% 
+  filter(grepl("\\(", album_name)) %>% 
+  filter(!grepl('Karaoke', album_name)) %>%
+  filter(!grepl('Big Machine Radio Release Special', album_name)) %>% 
+  filter(!grepl('Japanese Version', album_name)) %>% 
+  filter(!grepl('Deluxe_package', album_name)) %>% 
+  select(album_name) %>% 
+  distinct(album_name) %>% 
+  pull(album_name)
+albums_to_keep
+albums_to_keep <- c(albums_to_keep, 'reputation')
+albums_to_keep <- c(albums_to_keep, 'Taylor Swift')
+swift_condensed <- swift %>% 
+  filter(album_name %in% albums_to_keep) %>% distinct()
+View(swift_condensed)
+colnames(swift_condensed)
+swift_condensed <- swift_condensed %>% select(artist_name, album_type, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, time_signature, duration_ms, explicit, track_name, track_number, album_name, key_mode) %>% distinct()
+#add rep, combine 1989, add Taylor Swift debut, and keep deluxe sn 
