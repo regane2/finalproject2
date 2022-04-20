528897c9378aa11944e1e9ed3a13b4ce5fe3ee53
regane2 <92222931+regane2@users.noreply.github.com>
2022-04-20 15:52:07 -0700

    getting the goods

--- finalproject.R
+++ finalproject.R
@@ -1,2 +1,10 @@
 library(spotifyr)
 library(tidyverse)
+library(knitr)
+#getting the data 
+Sys.setenv(SPOTIFY_CLIENT_ID = 'c68051dec4174364a2748645aa18c878')
+Sys.setenv(SPOTIFY_CLIENT_SECRET = '61253abe841a49659ff9a901ad688e85')
+access_token <- get_spotify_access_token() 
+swift <- get_artist_audio_features('taylor swift')
+View(swift)
+#slicing and dicing 
