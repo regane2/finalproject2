e6c3ddf24d09425b8ca182a8efb6985d737d56a7
regane2 <92222931+regane2@users.noreply.github.com>
2022-05-04 15:10:38 -0700

    Adding more text

--- finalproject.R
+++ finalproject.R
@@ -1,15 +1,19 @@
-#Question: What is the most popular album and why? 
+#Question: What makes 1989 one of the most popular Taylor Swift albums and why? 
+#From someone who has 1989 at the Bottom of her tier ranking. (reputation supremacy)
+
 library(spotifyr)
 library(tidyverse)
 library(knitr)
+
 #getting the data 
 Sys.setenv(SPOTIFY_CLIENT_ID = 'c68051dec4174364a2748645aa18c878')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '61253abe841a49659ff9a901ad688e85')
 access_token <- get_spotify_access_token() 
 swift <- get_artist_audio_features('taylor swift')
 View(swift)
+
 #slicing and dicing 
-#I got rid of the original albums,live versions, Karaoke, Radio Release, and others so that it is down to the Deluxe Editions. There are duplicates but that's because I can't figure that out. Which. Fair enough. 
+#I got rid of the original albums, live versions, Karaoke, Radio Release, and others so that it is down to the Deluxe Editions. There are duplicates but that's because I can't figure that out. Which. Fair enough. 
 swift %>% distinct(album_name)
 albums_to_keep <- swift %>% 
   filter(grepl("\\(", album_name)) %>% 
@@ -31,7 +35,10 @@ swift_condensed <- swift %>%
 View(swift_condensed)
 colnames(swift_condensed)
 swift_condensed <- swift_condensed %>% select(artist_name, album_type, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, time_signature, duration_ms, explicit, track_name, track_number, album_name, key_mode) %>% distinct()
+
 #The Graphs (said in the same voice as the aliens saying the claw in Toy Story)
 ggplot(swift_condensed, aes(x= duration_ms, y = album_name)) + geom_point()
 ggplot(swift_condensed, aes(x= track_number, y= danceability, color = album_name)) + geom_point()
 ggplot(swift_condensed, aes(x= danceability, y= album_name)) + geom_point()
+#The decision 
+#
