65343bda8f55e0bcee8c890cdc5ae9546d190d51
regane2 <92222931+regane2@users.noreply.github.com>
2022-05-05 10:32:55 -0700

    adding

--- finalproject.R
+++ finalproject.R
@@ -10,7 +10,7 @@ Sys.setenv(SPOTIFY_CLIENT_ID = 'c68051dec4174364a2748645aa18c878')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '61253abe841a49659ff9a901ad688e85')
 access_token <- get_spotify_access_token() 
 swift <- get_artist_audio_features('taylor swift')
-View(swift)
+
 
 #slicing and dicing 
 #I got rid of the original albums, live versions, Karaoke, Radio Release, and others so that it is down to the Deluxe Editions. There are duplicates but that's because I can't figure that out. Which. Fair enough. 
@@ -33,10 +33,13 @@ albums_to_keep <- c(albums_to_keep, 'Taylor Swift')
 albums_to_keep <- c(albums_to_keep, "Lover")
 albums_to_keep <- c(albums_to_keep, "1989 (Deluxe Edition)")
 swift_condensed <- swift %>% 
-  filter(album_name %in% albums_to_keep) %>% distinct()
+  filter(album_name %in% albums_to_keep) %>% 
+  distinct()
 View(swift_condensed)
 colnames(swift_condensed)
-swift_condensed <- swift_condensed %>% select(artist_name, album_type, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, time_signature, duration_ms, explicit, track_name, track_number, album_name, key_mode) %>% distinct() 
+swift_condensed <- swift_condensed %>% 
+  select(artist_name, album_type, album_release_date, danceability, energy, key, loudness, mode, speechiness, acousticness, instrumentalness, liveness, valence, time_signature, duration_ms, explicit, track_name, track_number, album_name, key_mode) %>% 
+  distinct() 
 
 
 #The Plots (said in the same voice as the aliens saying the claw in Toy Story)
@@ -45,5 +48,5 @@ ggplot(swift_condensed, aes(x = danceability, y= album_name)) + geom_point()
 swift_condensed %>% group_by(album_name) %>% summarize(mean_danceability= mean(danceability)) %>% ggplot(aes(x=mean_danceability, y= reorder(album_name, mean_danceability))) + geom_col()
 
 #The decision 
-# People like 1989 the most because it is one of the most danceable albums! Also go me for predicting 1989 rerecord kinda! (she's releasing This love (TV) May 5th midnight!)
+# People like 1989 the most because it is one of the most danceable albums! Also go me for predicting 1989 rerecord kinda! (she's releasing This Love (Taylor's Version) May 5th midnight!)
 # 
